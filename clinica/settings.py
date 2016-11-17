af5fad1eedb909086ee60374931db30a3947fa2d
Marcos Castro <mcastrosouza@live.com>
2016-11-16 21:00:28 -0300

    adicionado coniguracoes locais

--- clinica/settings.py
+++ clinica/settings.py
@@ -116,10 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
